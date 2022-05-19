a7a3d7260b6555f32e9911819117f748c11ba4df
Carlos-vargs <cvargaslopez769@gmail.com>
2022-05-19 10:00:35 -0600

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,13 +22,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
 
-    Route::post('/logout', 'LogoutController@index');
+    Route::post('/logout', [LogoutController::class, 'index']);
+
+    Route::controller(CompanyController::class)->group(function () {
+        Route::get('/companies', 'index');
+        Route::post('/companies', 'store');
+        Route::put('/companies/{id}', 'update');
+        Route::delete('/companies/{id}', 'destroy');
+    });
+
+    Route::controller(ProductController::class)->group(function () {
+        Route::get('/products', 'index');
+        Route::post('/products', 'store');
+        Route::put('/products/{id}', 'update');
+        Route::delete('/products/{id}', 'destroy');
+    });
+
+    Route::controller(CategoryController::class)->group(function () {
+        Route::get('/categories', 'index');
+        Route::post('/categories', 'store');
+        Route::put('/categories/{id}', 'update');
+        Route::delete('/categories/{id}', 'destroy');
+    });
 
 
 });
 
-Route::post('/login', 'LoginController@index');
-Route::post('/register', 'RegisterController@index');
+Route::post('/login', [LoginController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'index']);
 
 
 
