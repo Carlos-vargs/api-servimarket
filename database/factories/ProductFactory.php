cb7be8cece07fa8c15500bee5c9df9be5af4761f
Carlos-vargs <cvargaslopez769@gmail.com>
2022-08-08 08:25:27 -0600

    :building_construction: Factories
    add factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => ucfirst($this->faker->words(4, true)),
+            'description' => $this->faker->paragraph(),
+            'created_at' => $this->faker->dateTimeBetween('-30 days', now())
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Company;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
+use Nette\Utils\Random;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +19,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        /*
+        $categories = Category::factory()->count(4);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $products = Product::factory()
+            ->hasAttached(rand(1, 50), ['rating' => rand(1, 5)], 'usersRated')
+            ->count(4);
+
+        Company::factory()
+            ->has($products)
+            ->count(50)
+            ->hasAttached($categories)
+            ->create();
+       */
+
+
+        // ->for($companies, 'company');
+        // ->hasAttached(User::factory()->count(6), ['rating' => 4], 'usersRated');
+
+        $products = Product::factory()
+            ->count(4);
+
+        $companies = Company::factory()
+            ->count(4)
+            ->hasAttached(Category::factory()->count(4))
+            ->has($products);
+
+        User::factory(50)
+            ->has($companies)
+            ->hasAttached(1, ['rating' => 1], 'productsRated')
+            ->create();
     }
 }
